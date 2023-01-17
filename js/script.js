986f7d32595a47177fb7e492276eb9b2e598380d
Mateusz Leszczyński <bxrq@wp.pl>
2023-01-17 14:48:43 +0100

    add JS function?

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-let formElement = document.querySelector(".js-form");
-let plnElement = document.querySelector(".js-pln");
-let usdElement = document.querySelector(".js-usd");
-let resultElement = document.querySelector(".js-result");
+{
+    const formElement = document.querySelector(".js-form");
+    const plnElement = document.querySelector(".js-pln");
+    const usdElement = document.querySelector(".js-usd");
+    const resultElement = document.querySelector(".js-result");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    let pln = plnElement.value;
-    let usd = usdElement.value;
-    let result = pln * usd;
-    resultElement.innerText = result.toFixed(2);
-})
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const pln = plnElement.value;
+        const usd = usdElement.value;
+        const result = pln * usd;
+        resultElement.innerText = result.toFixed(2);
+    })
+}
