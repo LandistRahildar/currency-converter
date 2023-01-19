d23d04eb4c83b18921c5b9d0a1048ebb854e22ee
Mateusz Leszczyński <bxrq@wp.pl>
2023-01-19 13:08:06 +0100

    js refactoring try

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 ## Demo
 https://landistrahildar.github.io/currency-converter/
 ## Description
-It's my first really simple currency converter from PLN to USD. For create this website I use HTML, CSS and JS.
+It's my first really simple currency converter from PLN to USD. For create this website I use HTML, CSS and JS. In code I use BEM convention.
 ## Presentation
 ![screen of the currency converter](images/presentation.png)
--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
+// {
+//     const formElement = document.querySelector(".js-form");
+
+//     formElement.addEventListener("submit", (event) => {
+//         const plnElement = document.querySelector(".js-pln");
+//         const usdElement = document.querySelector(".js-usd");
+//         const resultElement = document.querySelector(".js-result");
+//         event.preventDefault();
+//         const pln = plnElement.value;
+//         const usd = usdElement.value;
+//         const result = pln * usd;
+//         resultElement.innerText = result.toFixed(2);
+//     })
+// }
+
+
+
+
+
+
 {
     const formElement = document.querySelector(".js-form");
     const plnElement = document.querySelector(".js-pln");
     const usdElement = document.querySelector(".js-usd");
     const resultElement = document.querySelector(".js-result");
 
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const pln = plnElement.value;
-        const usd = usdElement.value;
-        const result = pln * usd;
-        resultElement.innerText = result.toFixed(2);
-    })
+    {
+        const calculateResoult = () => {
+            const pln = plnElement.value;
+            const usd = usdElement.value;
+            const result = pln * usd;
+        }
+    
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+            
+            resultElement.innerText = result.toFixed(2);
+        })
+    }
+
+
 }
