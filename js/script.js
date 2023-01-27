aedfbc6ddc00e69a9ebd146b84d764d341d8c00d
Mateusz Leszczyński <bxrq@wp.pl>
2023-01-27 11:52:32 +0100

    JS functoins revolution attemps (failed)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 {
     const formElement = document.querySelector(".js-form");
-    const plnElement = document.querySelector(".js-pln");
-    const usdElement = document.querySelector(".js-usd");
-    const resultElement = document.querySelector(".js-result");
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
+        const plnElement = document.querySelector(".js-pln");
+        const usdElement = document.querySelector(".js-usd");
+        const resultElement = document.querySelector(".js-result");
         const pln = plnElement.value;
         const usd = usdElement.value;
         const result = pln * usd;
@@ -22,6 +22,44 @@
 
 
 
+// {
+//     const formElement = document.querySelector(".js-form");
+//     const amountElement = document.querySelector(".js-amount");
+//     const currencyElement = document.querySelector(".js-currency");
+//     const resultElement = document.querySelector(".js-result");
+
+//     const rateEUR = 4.71;
+//     const rateGBP = 5.33;
+//     const rateUSD = 4.33;
+
+
+//     formElement.addEventListener("submit", (event) => {
+//         event.preventDefault();
+//     })
+
+//     const amount = +amountElement.value;
+//     const currency = currencyElement.value;
+
+//     let result;
+
+//     switch (currency) {
+//         case "EUR":
+//             result = amount / rateEUR;
+//             break;
+
+//         case "GBP":
+//             result = amount / rateGBP;
+//             break;
+
+//         case "USD":
+//             result = amount / rateUSD;
+//             break;
+//     }
+
+
+//     resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
+ 
+// }
 
 
 
@@ -29,7 +67,14 @@
 
 
 
-// szukaj fragmentów kodu, które robią konkretną rzecz, szukaj dla nich dobrych nazw funkcji 
+
+
+
+
+
+
+
+// szukaj fragmentów kodu, które robią konkretną rzecz, szukaj dla nich dobrych nazw funkcji
 
 // {
 //     const formElement = document.querySelector(".js-form");
